20eae212ed705b0e4e6d82b18dd2cc681451f81a
johnheywardobrien <reachjohnnow@gmail.com>
2015-05-04 22:37:50 +0000

    Tweaking .humanize functionality. Now displaying appropiate timestamp when viewing past calibrations

--- lib/models/calibration.rb
+++ lib/models/calibration.rb
@@ -1,7 +1,7 @@
 class Calibration < ActiveRecord::Base
   belongs_to :dream_color_monitor    # foreign key - dc_monitor_id
   
-  validates :luminance, format: { with: /\b\d{3}\.\d{2}\b/, message: "Please type in values like xx.xx" }
+  validates :luminance, format: { with: /\b\d{2,3}\.\d{2}\b/, message: "Please type in values like xx.xx" }
   validates :x_value, format: { with: /\A\.\d{4}\z/, message: "Please type valid input for X Value" }            
   validates :y_value, format: { with: /\A\.\d{4}\z/, message: "Please type valid input for Y Value" }            
   validates :attempts, inclusion: { in: 1..100, message: "Please enter a number from 0-100" }

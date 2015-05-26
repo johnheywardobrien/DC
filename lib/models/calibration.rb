d66926311359760b4bdbae88142fccc6e35e72bb
johnheywardobrien <reachjohnnow@gmail.com>
2015-05-26 01:47:20 +0000

    Change validation error to show helper for .xxxx

--- lib/models/calibration.rb
+++ lib/models/calibration.rb
@@ -2,8 +2,8 @@ class Calibration < ActiveRecord::Base
   belongs_to :dream_color_monitor    # foreign key - dc_monitor_id
   
   validates :luminance, format: { with: /\b\d{2,3}\.\d{2}\b/, message: "Please type in values like xx.xx" }
-  validates :x_value, format: { with: /\A\.\d{4}\z/, message: "Please type valid input for X Value" }            
-  validates :y_value, format: { with: /\A\.\d{4}\z/, message: "Please type valid input for Y Value" }            
+  validates :x_value, format: { with: /\A\.\d{4}\z/, message: "Please type valid input for X Value like .xxxx" }            
+  validates :y_value, format: { with: /\A\.\d{4}\z/, message: "Please type valid input for Y Value like .xxxx" }            
   validates :attempts, inclusion: { in: 1..100, message: "Please enter a number from 0-100" }
   validates :green, inclusion: { in: [true, false] }
   validates :date, presence: true
